7f5cb85bd42657fdc1638364d09a0cb1d41c759d
robot-bender <kjaniak444@gmail.com>
2023-09-29 22:28:19 +0200

    lambda

---
+++ src/main/java/org/example/lambda/Test.java
@@ -0,0 +1,13 @@
+package org.example.lambda;
+
+public class Test {
+    public static void main(String[] args) {
+        // lambda expression to implement above
+        // functional interface. This interface
+        // by default implements abstractFun()
+        FuncInterface fobj = (int x)-> System.out.println(2*x);
+
+        // This calls above lambda expression and prints 10
+        fobj.abstractFun(5);
+    }
+}
